ca74568b48710643cc1bc0de8398a8b8e347e6b4
Pintush Kumar <pintushmitcse@gmail.com>
2024-01-27 13:52:17 +0530

    all changes added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly rwonly = new RWOnly();
+    // rwonly.name = "Pintush";
+    //Facing an error - name' has private access in 'com.driver.RWOnly';
+
+    //rwonly.setName("Pintush");
+    //System.out.println(rwonly.getName());
 }
